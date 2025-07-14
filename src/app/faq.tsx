5d8687298767860684fdfc63cfdf9991ed543755
Lucas4413 <shi.chenghao@northeastern.edu>
2025-07-14 18:02:10 +0800

    修改常见问题的回答部分为默认打开

--- src/app/faq.tsx
+++ src/app/faq.tsx
@@ -5,8 +5,12 @@ import { Typography, Accordion, AccordionHeader, AccordionBody } from "@material
 import { FAQS } from "@/utility/FAQ";
 
 export function Faq() {
-  const [open, setOpen] = React.useState(0);
-  const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
+  // const [open, setOpen] = React.useState(0);
+  // const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
+  const [openQ1, setOpenQ1] = React.useState(true);
+  const [openQ2, setOpenQ2] = React.useState(true);
+  const handleOpenQ1 = (value: Boolean) => setOpenQ1(!value);
+  const handleOpenQ2 = (value: Boolean) => setOpenQ2(!value);
 
   return (
     <section className="py-8 px-8 lg:py-20">
@@ -24,7 +28,8 @@ export function Faq() {
         </div>
 
         <div className="mx-auto lg:max-w-screen-lg lg:px-20">
-          {FAQS.map(({ title, desc }, key) => (
+          {/* TODO：如果要放的问题过多则重构此处的代码变成按问题个数自动绑定状态来方便添加和维护 */}
+          {/* {FAQS.map(({ title, desc }, key) => (
             <Accordion
               key={key}
               open={open === key + 1}
@@ -42,7 +47,39 @@ export function Faq() {
                 </Typography>
               </AccordionBody>
             </Accordion>
-          ))}
+          ))} */}
+          <Accordion
+              open={openQ1 === true}
+              onClick={() => handleOpenQ1(openQ1)}
+            >
+              <AccordionHeader className="text-left text-gray-900">
+                {FAQS[0].title}
+              </AccordionHeader>
+              <AccordionBody>
+                <Typography
+                  color="blue-gray"
+                  className="font-normal text-gray-500"
+                >
+                  {FAQS[0].desc}
+                </Typography>
+              </AccordionBody>
+            </Accordion>
+            <Accordion
+              open={openQ2 === true}
+              onClick={() => handleOpenQ2(openQ2)}
+            >
+              <AccordionHeader className="text-left text-gray-900">
+                {FAQS[1].title}
+              </AccordionHeader>
+              <AccordionBody>
+                <Typography
+                  color="blue-gray"
+                  className="font-normal text-gray-500"
+                >
+                  {FAQS[1].desc}
+                </Typography>
+              </AccordionBody>
+            </Accordion>
         </div>
       </div>
     </section>
