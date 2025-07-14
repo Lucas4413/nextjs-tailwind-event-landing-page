bab8ad78fff7e0a558f91dc12d3245215fd5286a
Lucas4413 <shi.chenghao@northeastern.edu>
2025-07-14 18:05:45 +0800

    “调整配置文件，使响应式布局的窗口大小阈值相同”

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,18 @@ const config: Config = withMT({
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '720px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    },
+  },
   plugins: [
     require('tailwind-scrollbar-hide'),
     require('@tailwindcss/typography')
