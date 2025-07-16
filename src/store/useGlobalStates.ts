145d1fa07be108c8ae460f41f2b030e8c44a47b1
Lucas4413 <shi.chenghao@northeastern.edu>
2025-07-16 17:34:35 +0800

    建立全局state库用于全局状态管理

---
+++ src/store/useGlobalStates.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+
+// 用zustand定义全局状态和状态管理函数
+
+type UserInfo = {
+  name: string;
+  email: string;
+  count: number;
+};
+
+type GlobalState = {
+  userInfo: UserInfo | null;
+  setUserInfo: (info: UserInfo) => void;
+  updateUserInfo: (partial: Partial<UserInfo>) => void;
+  isLoggedIn: boolean;
+  setLoggedIn: (status: boolean) => void;
+};
+
+export const useGlobalStates = create<GlobalState>((set) => ({
+  userInfo: null,
+  isLoggedIn: false,
+  setUserInfo: (info) => set(() => ({ userInfo: info })),
+  updateUserInfo: (partial) =>
+    set((state) => ({
+      userInfo: state.userInfo ? { ...state.userInfo, ...partial } : null,
+    })),
+  setLoggedIn: (status) => set(() => ({ isLoggedIn: status })),
+}));
